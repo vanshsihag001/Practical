6cff90db753658f9374a34652bab705b53cb5f98
Vansh Sihag <vanshsihag001@gmail.com>
2026-05-07 12:34:23 +0530

    Update App.jsx

--- q-2/src/App.jsx
+++ q-2/src/App.jsx
@@ -34,24 +34,5 @@ function App() {
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
-=======
-import { useState } from "react";
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>Counter App</h1>
-
-      <h2>Count: {count}</h2>
-
-      <button onClick={() => setCount(count + 1)}>Increment Button </button>
-      <button onClick={() => setCount(count - 1)}>Decrement Button</button>
-      <button onClick={() => setCount(0)}>Reset Button</button>
-    </div>
->>>>>>> a913a4e845e30b9a5f215e7bb050695df31fcff8
-  );
-}
 
 export default App;
